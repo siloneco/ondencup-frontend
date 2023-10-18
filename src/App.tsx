0f447b3158b6a6ba7e67fc4d5740cf9342636e69
siloneco <siloneco4646@gmail.com>
2023-10-18 21:57:47 +0900

    feat: just be able to connect livekit server

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '@livekit/components-styles';
+import {
+  LiveKitRoom,
+  GridLayout,
+  ParticipantTile,
+  ControlBar,
+  RoomAudioRenderer,
+  useTracks
+} from '@livekit/components-react';
+
+const serverUrl = 'REDACTED';
+const token = 'REDACTED';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <LiveKitRoom
+      video={true}
+      audio={true}
+      token={token}
+      connectOptions={{ autoSubscribe: false }}
+      serverUrl={serverUrl}
+      // Use the default LiveKit theme for nice styles.
+      data-lk-theme="default"
+      style={{ height: '100vh', width: '100vw' }}
+    >
+      <MyVideoConference />
+      <RoomAudioRenderer />
+      <ControlBar />
+    </LiveKitRoom>
+  );
 }
 
-export default App
+function MyVideoConference() {
+  // `useTracks` returns all camera and screen share tracks. If a user
+  // joins without a published camera track, a placeholder track is returned.
+  const tracks = useTracks(
+    undefined,
+    { onlySubscribed: false },
+  );
+  return (
+    <GridLayout tracks={tracks} style={{ height: 'calc(100vh - var(--lk-control-bar-height))' }}>
+      {/* The GridLayout accepts zero or one child. The child is used
+      as a template to render all passed in tracks. */}
+      <ParticipantTile />
+    </GridLayout>
+  );
+}
+
+export default App;
